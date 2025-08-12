31c9d827add06aaf77faf28b0bff01e26d1fdfb9
Navnidhi <nnavnidhi_be23@thapar.edu>
2025-08-12 15:14:22 +0530

    Update pandas_assignment2.py

--- pandas_assignment2.py
+++ pandas_assignment2.py
@@ -69,7 +69,62 @@ df.iloc[:,1:4]
 
 
 
-# In[ ]:
+data = {
+    "Employee_ID": [101, 102, 103, 104, 105],
+    "Name": ["Alice", "Bob", "Charlie", "Diana", "Edward"],
+    "Department": ["HR", "IT", "IT", "Marketing", "Sales"],
+    "Age": [29, 34, 41, 28, 38],
+    "Salary": [50000, 70000, 65000, 55000, 60000],
+    "Years_of_Experience": [4, 8, 10, 3, 12],
+    "Joining_Date": ["2020-03-15", "2017-07-19", "2013-06-01", "2021-02-10", "2010-11-25"],
+    "Gender": ["Female", "Male", "Male", "Female", "Male"],
+    "Bonus": [5000, 7000, 6000, 4500, 5000],
+    "Rating": [4.5, 4.0, 3.8, 4.7, 3.5]
+}
+
+df = pd.DataFrame(data)
+df.to_csv("employees.csv", index=False)
+
+import pandas as pd
+df = pd.read_csv("employees.csv")
+
+print("Shape ", df.shape)
+
+print(df.info())
+
+print(df.describe())
+
+print(df.head())
+
+print(df.tail(3))
+
+average= df["Salary"].mean()
+total = df["Bonus"].sum()
+youngest = df["Age"].min()
+highest = df["Rating"].max()
+
+print("Average", average)
+print("Total", total)
+print("Youngest ", youngest)
+print(, highest)
+
+sortdf = df.sort_values(by="Salary", ascending=False)
+
+print(sortdf)
+print("\nMissing Values ")
+print(df.isnull().sum())
+
+df.rename(columns={"Employee_ID": "ID"}, inplace=True)
+print(df.head())
+
+print(df[df["Years_of_Experience"] > 5])
+
+print(df[df["Department"] == "IT"])
+df["Tax"] = df["Salary"] * 0.1
+
+print(df)
+
+df.to_csv("modified_employees.csv", index=False)
 
 
 
